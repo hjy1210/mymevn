ca4510c0a3cae35a62fbde2b89797d3739e0a2cd
hjyanghj <hjyanghj@gmail.com>
2020-10-21 16:13:29 +0800

    Create nav-bar for Vue application

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+ <div >
+  <nav >
+   <ul >
+    <li >
+     <router-link to="/" >Home</router-link>
+    </li>
+    <li >
+     <router-link to="/create" >Create Post</router-link>
+    </li>
+    <li >
+     <router-link to="/posts" >Posts</router-link>
+    </li>
+   </ul>
+  </nav><br />
+  <transition name=fade>
+   <router-view></router-view>
+  </transition>
+ </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
+
+<script>
+
+  export default{
+  }
+</script>
