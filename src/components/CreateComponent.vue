509e4044cc66d4288b8bb1f8e8f1f2c13c90d995
hjyanghj <hjyanghj@gmail.com>
2020-10-28 22:53:01 +0800

    add addPostAsync

--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -1,45 +1,51 @@
 <template>
- <div>
-  <h1>Create A Post</h1>
-  <form @submit.prevent="addPost">
-   <div >
-    <div >
-     <div >
-      <label>Post Title:</label>
-      <input type=text v-model="post.title">
-     </div>
-    </div>
-    </div>
-    <div >
-     <div >
-      <div >
-       <label>Post Body:</label>
-       <textarea v-model="post.body" rows="5"></textarea>
+  <div>
+    <h1>Create A Post</h1>
+    <form @submit.prevent="addPostAsync">
+      <div>
+        <div>
+          <div>
+            <label>Post Title:</label>
+            <input type="text" v-model="post.title" />
+          </div>
+        </div>
       </div>
-     </div>
-    </div><br />
-    <div >
-     <button >Create</button>
-    </div>
-  </form>
- </div>
+      <div>
+        <div>
+          <div>
+            <label>Post Body:</label>
+            <textarea v-model="post.body" rows="5"></textarea>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div>
+        <button>Create</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-  export default {
-    data(){
+export default {
+  data() {
     return {
-     post:{}
-    }
+      post: {},
+    };
   },
   methods: {
-    addPost(){
-      let uri = '//localhost:4000/posts/add';
+    addPost() {
+      let uri = "//localhost:4000/posts/add";
       this.axios.post(uri, this.post).then(() => {
-        this.$router.push({name: 'posts'});
+        this.$router.push({ name: "posts" });
       });
-     //console.log(this.post);
+    },
+    async addPostAsync() {
+      let uri = "//localhost:4000/posts/add";
+      let r = await this.axios.post(uri, this.post);
+      console.log(r);
+      this.$router.push({ name: "posts" });
     }
   }
- }
+};
 </script>
